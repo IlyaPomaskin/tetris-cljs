a629b13c56bb3313596adab4df44458746fd25bb
Ilya Pomaskin <ilya-pomaskin@yandex-team.ru>
2019-04-01 01:25:34 +0300

    initial commit

---
+++ src/main/tetris/tetrominos.cljs
@@ -0,0 +1,39 @@
+(ns tetris.tetrominos)
+
+(def tetromino-i
+  [[1]
+   [1]
+   [1]
+   [1]])
+(def tetromino-z
+  [[0 2]
+   [2 2]
+   [2 0]])
+(def tetromino-s
+  [[3 0]
+   [3 3]
+   [0 3]])
+(def tetromino-o
+  [[4 4]
+   [4 4]])
+(def tetromino-t
+  [[0 5]
+   [5 5]
+   [0 5]])
+(def tetromino-j
+  [[0 6]
+   [0 6]
+   [6 6]])
+(def tetromino-l
+  [[7 0]
+   [7 0]
+   [7 7]])
+
+(def items
+  [tetromino-i
+   tetromino-z
+   tetromino-s
+   tetromino-o
+   tetromino-t
+   tetromino-j
+   tetromino-l])
