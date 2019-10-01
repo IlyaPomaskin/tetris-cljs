5196d783472c16738e1d4f6752f1debcf26515f1
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-10-01 19:08:42 +0300

    SRS tetrominoes

--- src/main/tetris/game.cljs
+++ src/main/tetris/game.cljs
@@ -23,7 +23,7 @@
         [[piece] next-buffer] (split-at 1 buffer)]
     (-> state
         (assoc :buffer (if (empty? next-buffer)
-                         (shuffle tetrominoes/items)
+                         (shuffle tetrominoes/srs-items)
                          next-buffer))
         (assoc :piece (make-piece piece (/ field-width 2))))))
 
@@ -32,7 +32,7 @@
   (use-next-piece
    {:stack (vec (repeat field-height (vec (repeat field-width 0))))
     :piece nil
-    :buffer (shuffle tetrominoes/items)
+    :buffer (shuffle tetrominoes/srs-items)
     :score 0
     :lines 0
     :state :game}))
@@ -43,7 +43,7 @@
          piece-x :x
          piece-y :y} piece]
     (for [[cell-y line] (map-indexed vector cells)
-          [cell-x cell] (map-indexed vector line)
+          [cell-x] (map-indexed vector line)
           :let [stack-y (+ piece-y cell-y)
                 stack-x (+ piece-x cell-x)]
           :when (not (zero? (get-in cells [cell-y cell-x])))]
@@ -54,29 +54,25 @@
   (= (get state :state) :game-over))
 
 
+(defn out-of-bounds? [stack piece]
+  (let [stack-height (count stack)
+        stack-width (count (peek stack))]
+    (->> (piece->coords piece)
+         (filter
+          (fn [[y x]]
+            (or (>= y stack-height)
+                (>= x stack-width)
+                (< x 0))))
+         (first)
+         (some?))))
+
+
 (defn has-collisions? [stack piece]
   (->> (piece->coords piece)
        (map #(get-in stack %1))
        (filter pos?)
        (first)
        (some?)))
-
-
-(defn out-of-bounds? [stack piece]
-  (let [{cells :cells
-         piece-x :x
-         piece-y :y} piece
-        piece-height (count cells)
-        stack-height (count stack)
-        out-of-y-bound? (> (+ piece-y piece-height)
-                           stack-height)
-        piece-width (count (peek cells))
-        stack-width (count (peek stack))
-        out-of-x-bound? (or (> (+ piece-x piece-width)
-                               stack-width)
-                            (< (+ piece-x piece-width)
-                               0))]
-    (or out-of-y-bound? out-of-x-bound?)))
 
 
 (defn place-piece [state]
@@ -86,11 +82,10 @@
                           (flatten)
                           (filter #(not (zero? %1)))
                           (first))
-        piece-coords (piece->coords piece)
         next-stack (reduce
                     (fn [stack [y x]] (assoc-in stack [y x] piece-symbol))
                     stack
-                    piece-coords)]
+                    (piece->coords piece))]
     (assoc state :stack next-stack)))
 
 
@@ -118,12 +113,9 @@
 
 
 (defn move-piece [offset state]
-  (let [piece (get state :piece)
-        stack (get state :stack)
-        piece-width (count (peek (get piece :cells)))
-        offseted-x (+ (get piece :x) offset)
-        stack-width (count (peek stack))
-        next-x (max 0 (min (- stack-width piece-width) offseted-x))
+  (let [{stack :stack
+         piece :piece} state
+        next-x (+ (get piece :x) offset)
         next-piece (assoc piece :x next-x)]
     (if (or (has-collisions? stack next-piece)
             (out-of-bounds? stack next-piece))
@@ -197,11 +189,12 @@
 (defn fall [state]
   (let [stack (get state :stack)
         current-piece (get state :piece)
-        lowered-piece (update-in current-piece [:y] inc)]
-    (if (or (has-collisions? stack lowered-piece)
-            (out-of-bounds? stack lowered-piece))
+        next-piece (update-in current-piece [:y] inc)]
+    (if (or
+         (has-collisions? stack next-piece)
+         (out-of-bounds? stack next-piece))
       (next-cycle state)
-      (assoc-in state [:piece] lowered-piece))))
+      (assoc-in state [:piece] next-piece))))
 
 
 (defn hard-drop [state]
--- src/main/tetris/tetrominoes.cljs
+++ src/main/tetris/tetrominoes.cljs
@@ -26,3 +26,88 @@
 
 (def items
   [i z s o t j l])
+
+(def l-piece [[1 0 0]
+              [1 1 1]
+              [0 0 0]])
+(def j-piece [[0 0 2]
+              [2 2 2]
+              [0 0 0]])
+(def s-piece [[0 3 3]
+              [3 3 0]
+              [0 0 0]])
+(def t-piece [[0 4 0]
+              [4 4 4]
+              [0 0 0]])
+(def z-piece [[5 5 0]
+              [0 5 5]
+              [0 0 0]])
+(def o-piece [[6 6]
+              [6 6]])
+(def i-piece [[0 0 0 0]
+              [7 7 7 7]
+              [0 0 0 0]
+              [0 0 0 0]])
+(def srs-items [l-piece
+                j-piece
+                s-piece
+                t-piece
+                z-piece
+                o-piece
+                i-piece])
+
+(def wall-kick-0->R
+  [[0 0]
+   [-1 0]
+   [-1 1]
+   [0 -2]
+   [-1 -2]])
+
+(def wall-kick-R->2
+  [[0 0]
+   [1 0]
+   [1 -1]
+   [0 2]
+   [1 2]])
+
+(def wall-kick-2->L
+  [[0 0]
+   [1 0]
+   [1 1]
+   [0 -2]
+   [1 -2]])
+
+(def wall-kick-L->0
+  [[0 0]
+   [-1 0]
+   [-1 -1]
+   [0 2]
+   [-1 2]])
+
+(def wall-kick-i-0->R
+  [[0 0]
+   [-2 0]
+   [1 0]
+   [-2 -1]
+   [1 2]])
+
+(def wall-kick-i-R->2
+  [[0 0]
+   [-1 0]
+   [2 0]
+   [-1 2]
+   [2 -1]])
+
+(def wall-kick-i-2->L
+  [[0 0]
+   [2 0]
+   [-1 0]
+   [2 1]
+   [-1 -2]])
+
+(def wall-kick-i-L->0
+  [[0 0]
+   [1 0]
+   [-2 0]
+   [1 -2]
+   [-2 1]])
