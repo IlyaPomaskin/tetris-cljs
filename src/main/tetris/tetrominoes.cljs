69e1b0c117c81667077aa859657772af2df16c7c
Ilya Pomaskin <ilya.pomaskin@gmail.com>
2019-11-18 22:17:11 +0300

    remove unused items

--- src/main/tetris/tetrominoes.cljs
+++ src/main/tetris/tetrominoes.cljs
@@ -1,35 +1,6 @@
 (ns tetris.tetrominoes)
 
 
-(def i
-  [[1]
-   [1]
-   [1]
-   [1]])
-(def z
-  [[2 2 0]
-   [0 2 2]])
-(def s
-  [[0 3 3]
-   [3 3 0]])
-(def o
-  [[4 4]
-   [4 4]])
-(def t
-  [[5 5 5]
-   [0 5 0]])
-(def j
-  [[6 6 6]
-   [0 0 6]])
-(def l
-  [[7 7 7]
-   [7 0 0]])
-
-
-(def items
-  [i z s o t j l])
-
-
 (def l-piece
   {:name :l
    :0 [[1 0 0]
