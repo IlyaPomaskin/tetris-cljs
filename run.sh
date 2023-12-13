98c3f00a2af1dffdfddec911f1b6a13dfb6cde95
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-12-13 17:01:57 +0500

    expose repl port

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ docker run \
     -v m2-cache:/home/circleci/.m2/repository \
     -p 127.0.0.1:3000:3000 \
     -p 127.0.0.1:9000:9000 \
+    -p 127.0.0.1:9001:9001 \
     -p 127.0.0.1:9630:9630 \
     -it tetris-cljs
