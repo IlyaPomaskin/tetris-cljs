3bc90acff360e3db3cf039629b668e46ba9d4a8e
Ilya Pomaskin <IlyaPomaskin@users.noreply.github.com>
2023-12-17 13:24:21 +0500

    tune colors

--- src/main/tetris/ui.cljs
+++ src/main/tetris/ui.cljs
@@ -7,6 +7,8 @@
 (def canvas (js/document.querySelector "#canvas"))
 (def ctx (.getContext canvas "2d"))
 
+(set! (.-imageSmoothingEnabled ctx) false)
+
 (def cell-size 20)
 (def next-piece-cell-size 10)
 
@@ -16,17 +18,26 @@
 (def field-width (* 10 cell-size))
 (def field-height (* 22 cell-size))
 
+(def black "black")
+
 (defn cell->color [cell]
   (case cell
-    :l "#f0a000"
-    :j "#0000f0"
-    :s "#00f000"
-    :t "#a000f0"
-    :z "#f00000"
-    :o "#f0f000"
-    :i "#0793f7"
+    ; orange
+    :l "#ff5714"
+    ; blue
+    :j "#008bf8"
+    ; green
+    :s "#04e762"
+    ; purple
+    :t "#9016d2"
+    ; red
+    :z "#dc0073"
+    ; yellow
+    :o "#faff00"
+    ; cyan
+    :i "#1be7ff"
     :g "rgba(255, 255, 255, 0.3)"
-    "black"))
+    black))
 
 (defn draw-text! [x y size text]
   (set! (.-strokeStyle ctx) "white")
@@ -34,9 +45,14 @@
   (set! (.-font ctx) (string/join ["bold " size "px sans-serif"]))
   (.fillText ctx text x y))
 
-(defn draw-border! [x y w h color]
-  (set! (.-strokeStyle ctx) color)
-  (.strokeRect ctx x y w h))
+(defn draw-border!
+  ([x y w h color]
+   (draw-border! x y w h 1 color))
+
+  ([x y w h width color]
+   (set! (.-lineWidth ctx) width)
+   (set! (.-strokeStyle ctx) color)
+   (.strokeRect ctx x y w h)))
 
 (defn draw-inner-border! [x y w h color]
   (draw-border! (inc x) (inc y) (dec w) (dec h) color))
@@ -45,21 +61,52 @@
   (set! (.-fillStyle ctx) color)
   (.fillRect ctx x y w h))
 
+(defn draw-line! [x1 y1 x2 y2 color]
+  (set! (.-strokeStyle ctx) color)
+  (.beginPath ctx)
+  (.moveTo ctx x1 y1)
+  (.lineTo ctx x2 y2)
+  (.stroke ctx))
+
 (defn draw-cell!
   ([x y piece]
    (draw-cell! x y piece cell-size))
 
   ([x y piece size]
+; (set! (.-shadowBlur ctx) 0)
+; (set! (.-shadowColor ctx) "transparent")
+
+; (set! (.-filter ctx) "drop-shadow(4px 3px 4px #000000 ) ")
+   (set! (.-filter ctx) "none")
+
    (when piece
-     (draw-rect! (* x size) (* y size) size size (cell->color piece))
-     (draw-border! (* x size) (* y size) size size "black"))))
+     (let [x' (* x size)
+           y' (* y size)]
+       (draw-rect! x' y' size size (cell->color piece))
+
+       (draw-rect! (+ 1 x') (+ 2 y') (- size 0) (- size 0) "rgba(0,0,0,0.2)")
+
+       ; (draw-line! (inc x') y' x' (+ y' size) "rgba(255,255,255,0.8)")
+       ; (draw-line! x' (inc y') (+ x' size) y' "rgba(255,255,255,0.8)")
+       ; (draw-line! (+ x' -1  size) y' (+ x' -1 size) (+ y' size) "rgba(0,0,0,0.3)")
+
+       ; (draw-line! (inc x') y' x' (+ y' size) "white")
+       ; (draw-line! x' (inc y') (+ x' size) y' "white")
+       ; (draw-line! (+ x' size) y' (+ x' size) y' "red")
+
+       ; (draw-rect! x' y' 2 2 "white")
+       ; (draw-rect! (+ x' 2) (+ y' 2) 2 2 "white")
+       ; (draw-rect! (+ x' 2) (+ y' 4) 2 2 "white")
+       ; (draw-rect! (+ x' 4) (+ y' 2) 2 2 "white")
+
+       (draw-border! x' y' size size "rgba(0,0,0,0.2)")))))
 
 (defn render-stack [stack]
   (utils/iterate-stack
    stack
    (fn [x y]
      (draw-border! (* x cell-size) (* y cell-size) cell-size cell-size "white")))
-  (draw-rect! 0 0 canvas-width canvas-height "rgba(0, 0, 0, 0.8)")
+  (draw-rect! 0 0 canvas-width canvas-height black)
   (utils/iterate-stack stack draw-cell!)
   (draw-inner-border! 0 (* 2 cell-size) field-width (- field-height (* 2 cell-size)) "green"))
 
@@ -99,7 +146,8 @@
 
 (defn render-game [_ _ _ state]
   (let [next-state (game/place-piece (game/place-ghost-piece state))]
-    (.clearRect ctx 0 0 canvas-width canvas-height)
+    ; (.clearRect ctx 0 0 canvas-width canvas-height)
+    (draw-rect! 0 0 canvas-width canvas-height black)
     (render-stack (:stack next-state))
     (render-next-pieces (:buffer next-state))
     (render-stats state)
